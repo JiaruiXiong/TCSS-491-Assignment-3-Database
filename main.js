1eabd83a0de7f2bc712154f110fda476e616a854
JiaruiXiong <56058784+JiaruiXiong@users.noreply.github.com>
2020-02-28 00:09:35 -0800

    。

--- main.js
+++ main.js
@@ -1,40 +1,40 @@
-// window.onload = function () {
-//   var socket = io.connect("http://24.16.255.56:8888");  // connect to database
+window.onload = function () {
+  var socket = io.connect("http://24.16.255.56:8888");  // connect to database
 
-//   socket.on("load", function (data) {
-//       console.log(data);
-//   });
+  socket.on("load", function (data) {
+      console.log(data);
+  });
 
-//   var text = document.getElementById("text");
-//   var saveButton = document.getElementById("save");
-//   var loadButton = document.getElementById("load");
+  var text = document.getElementById("text");
+  var saveButton = document.getElementById("save");
+  var loadButton = document.getElementById("load");
 
-//   saveButton.onclick = function () {
-//     console.log("save");
-//     text.innerHTML = "Saved."
-//     // io.sockets[0].emit();  
-//     socket.emit("save", { studentname: "Chris Marriott", statename: "aState", data: "Goodbye World" }); //save format // blockcast 
-//   };
+  saveButton.onclick = function () {
+    console.log("save");
+    text.innerHTML = "Saved."
+    // io.sockets[0].emit();  
+    socket.emit("save", { studentname: "Chris Marriott", statename: "aState", data: "Goodbye World" }); //save format // blockcast 
+  };
 
-//   loadButton.onclick = function () {
-//     console.log("load");
-//     text.innerHTML = "Loaded."
-//     socket.emit("load", { studentname: "Chris Marriott", statename: "aState" });
-//   };
+  loadButton.onclick = function () {
+    console.log("load");
+    text.innerHTML = "Loaded."
+    socket.emit("load", { studentname: "Chris Marriott", statename: "aState" });
+  };
 
-//   // // 我加的
-//   socket.on("connect", function() {
-//     console.log("Connected");
-//   })
+  // // 我加的
+  socket.on("connect", function() {
+    console.log("Connected");
+  })
 
-//   socket.on("dicconnect", function() {
-//     console.log("Disconnected");
-//   })
+  socket.on("dicconnect", function() {
+    console.log("Disconnected");
+  })
   
 
-//   // socket.emit("mesasge",);
+  // socket.emit("mesasge",);
 
-// };
+};
 
 
 var AM = new AssetManager();
